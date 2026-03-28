c2d6c5d51f15c4d4438df0229143425288339f80
akashanand07 <akashanand33qq@gmail.com>
2026-03-28 14:09:48 +0530

    Update app.py

--- stock-market/app.py
+++ stock-market/app.py
@@ -1,1 +1,13 @@
+import yfinance as yf
+import pandas as pd
+import time
 
+while True:
+    stock = yf.Ticker("AAPL")
+    data = stock.history(period="1d", interval="1m")
+
+    data.to_csv("stock_data.csv")
+
+    print("Updated")
+
+    time.sleep(30)

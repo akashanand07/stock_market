e1e5ab55935c8216bf88d25a3a52b5791d63b7a3
akashanand07 <akashanand33qq@gmail.com>
2026-03-28 15:12:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import streamlit as st
 import yfinance as yf
 import pandas as pd
-import time
 
-while True:
-    stock = yf.Ticker("AAPL")
+st.title("📈 Live Stock Dashboard")
+
+# user input
+ticker = st.text_input("Enter Stock Symbol", "AAPL")
+
+# caching to avoid rate limit
+@st.cache_data(ttl=60)
+def load_data(ticker):
+    stock = yf.Ticker(ticker)
     data = stock.history(period="1d", interval="1m")
+    return data
 
-    data.to_csv("stock_data.csv")
+# load data
+data = load_data(ticker)
 
-    print("Updated")
-
-    time.sleep(30)
+# display
+st.write(data)
+st.line_chart(data["Close"])
